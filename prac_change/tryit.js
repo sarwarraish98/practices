442599e242ad69a4ef9f68d42b6cbf1402570504
md sarwar raish <sarwarraish98@gmail.com>
2025-02-17 14:03:57 +0530

    commit

---
+++ prac_change/tryit.js
@@ -0,0 +1,158 @@
+// // let c = 12;
+// // let d = 23;
+
+// // let age =26;
+// // //  let variable means container which can change future
+// // // constant will  
+// // let name = "Raees";
+// // console.log('age',age);
+// //  age= 28;
+
+
+// // console.log("c+d is ",c+d);
+// // console.log('age',age);
+// // console.log('name',name);
+// // console.log(c + d);
+
+// // let a=23;
+// // console.log('the value of a',age);
+
+// // //** */ types of data types
+// // -String 
+// // -Number
+// // -Boolean
+// // -undefined
+// // -Object
+
+
+// // lecture 2 Anuj
+// // let a =2;
+// // a ='Name'
+// // a= false 
+// // console.log('the value of a is',a);
+// // console.log('the type of a is ',typeof(a));
+
+
+// //type conversion in javascript
+// let a=23;
+// a='23';
+// a= Boolean(a);
+// console.log('the value of a is',a);
+// console.log('the type of a is ',typeof(a));
+// // HTML adds Structure to a web page, CSS styles it and JavaScript brings
+// //  it to life by allowing users to interact with elements on the page,
+// //  such as actions on clicking buttons, filling out forms, and showing
+// //  animations
+
+// //operator
+
+// const x = 3;
+// const y = 4;
+ 
+// const z =  x+ y;
+// console.log (z);
+
+// let firstname = 4;
+// let lastname = 5;
+// let result =(firstname&&lastname)
+
+// console.log(result);
+
+// //conditional operator
+
+// let number = 2;
+// if(number >= 0){
+//     //code
+//     console.log(number)
+    
+// }
+// else{
+//     //code
+//     console.log(number)
+
+// }
+// //code after if 
+// if(number < 0 ){
+//     console.log(number*4);
+// }
+
+// else{
+//     console.log(number/2);
+// }
+
+// //ternary operator 
+// // a ternary operator evaluates a condition and executes a block of code based on the condition.
+
+// // for(let i=0; i<=100;i++){
+// //     console.log(i);
+// // }
+
+// //mao
+
+// // const m = new Map();
+// // // hello brother 
+// // m.set('name', 'GFG');
+// // m.set('Rank', 1);
+// // console.log(m.size); 
+
+
+// //switch statement
+// // hte javascript switch statement is used in decision making .the swith
+// // evaluates an expression and executes the corresponding body that matches the expression's result.
+
+// let grade = 2
+
+// switch(grade){
+//     case 1:
+//         console.log("hello");
+//         break;
+//     case 2:
+//         console.log("brother");
+//         break;
+//     case 3:
+//         console.log("good morning");
+//         break;
+//     default:
+//         console.log("all done");
+//         break;
+
+// }
+
+
+// for loop 
+ 
+for( let i=0; i<=5; i++)
+    {
+    console.log("Raees" ,i);
+
+}
+let step = 0 ;
+while(step < 5  ){
+
+    console.log("sarwar",step);
+   step +=1;
+}
+
+
+//do while loop
+
+let way =0;
+do{
+    
+   
+    // if(way===2){
+    //     continue;
+       
+    // }
+    console.log('way',way);
+    way +=1;
+
+} while(way<5);
+
+
+/// array 
+// an array is an object that can store multiple values at once.
+
+const words = ["apple","banana","cherry",12];
+console.log(words);
+// give me
